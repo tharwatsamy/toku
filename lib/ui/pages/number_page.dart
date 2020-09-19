2a17460b744e3008ba814c4f1eed103451f8280e
Tharwat samy <tharwatsamy2004@gmail.com>
2020-09-19 19:36:15 +0200

    commit

---
+++ lib/ui/pages/number_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:toku/models/number.dart';
+import 'package:toku/ui/components/custom_app_bar.dart';
+import 'package:toku/ui/components/custom_container.dart';
+
+class NumbersPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(title: 'Numbers'),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          print('numbersList = ${numbersList[index].music}');
+          print(index);
+          return CustomContainer(
+            music: numbersList[index].music,
+            color: Color(0xFFEF9234),
+            image: numbersList[index].image,
+            englishName: numbersList[index].englishName,
+            japaneseName: numbersList[index].japaneseName,
+          );
+        },
+        itemCount: numbersList.length,
+      ),
+    );
+  }
+}
